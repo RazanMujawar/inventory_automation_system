81e2fb313266ade78a70e5162189f939b9c2a7fe
razanmujawar2211-create <razanmujawar2211@gmail.com>
2026-06-03 19:24:34 +0530

    Removing unwanted file

--- database/db_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def get_connection():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="@Razan2211",
-        database="inventory_db"
-    )
-    return connection
