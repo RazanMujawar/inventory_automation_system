9b327608f9059ea8a5bac1f37ac5ea659a163c3f
razanmujawar2211-create <razanmujawar2211@gmail.com>
2026-06-15 17:30:39 +0530

    Fix logger for Streamlit Cloud

--- modules/logger.py
+++ modules/logger.py
@@ -1,9 +1,8 @@
 import logging
 
 logging.basicConfig(
-    filename = "logs/app.log",
-    level = logging.INFO,
-    format = "%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 logger = logging.getLogger(__name__)
