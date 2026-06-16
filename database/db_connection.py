8d806faf2b975d3533a40e2b0b2e8dcdee7a20ac
razan.mujawar@optimumdataanalytics.com <razanmujawar2211@gmail.com>
2026-06-16 13:35:46 +0530

    Remove connection pool

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
-from mysql.connector.pooling import MySQLConnectionPool
+import mysql.connector
 
 load_dotenv()
 
@@ -11,21 +11,12 @@ def get_secret(key):
     except:
         return os.getenv(key)
 
-host=get_secret("DB_HOST")
-port=int(get_secret("DB_PORT"))
-user=get_secret("DB_USER")
-password=get_secret("DB_PASSWORD")
-database=get_secret("DB_NAME")
-
-
-pool = MySQLConnectionPool(
-    pool_name="inventory_pool",
-    pool_size=5,
-    host=host,
-    port=port,
-    user=user,
-    password=password,
-    database=database
-)
 def get_connection():
-    return pool.get_connection()
+
+    return mysql.connector.connect(
+        host=get_secret("DB_HOST"),
+        port=int(get_secret("DB_PORT")),
+        user=get_secret("DB_USER"),
+        password=get_secret("DB_PASSWORD"),
+        database=get_secret("DB_NAME")
+    )
